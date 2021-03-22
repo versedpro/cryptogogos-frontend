388cb36b692e720117d4315587c3d0c73fbbf063
umar-shafiq <umarshafiq2012@gmail.com>
2021-03-22 15:35:03 +0500

    added fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Mar 30, 2021 12:00:00").getTime();
+var countDownDate = new Date("Apr 01, 2021 18:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
@@ -17,7 +17,7 @@ var x = setInterval(function () {
 
   // Display the result in the element with id="demo"
   document.getElementById("timer").innerHTML =
-    days + ": " + hours + ": " + minutes;
+    days + ": " + hours + ": " + minutes + ": " + seconds;
 
   // If the count down is finished, write some text
   if (distance < 0) {
