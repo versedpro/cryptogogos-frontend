1d24db4539a547d6076c91057b356c59ecc53205
umar-shafiq <umarshafiq2012@gmail.com>
2021-04-02 20:57:07 +0500

    build added

---
+++ src/components/DrawTrade.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function DrawTrade() {
+return(
+    <div class="draw-trade"> Draw trade</div>
+);
+}
+
+export default DrawTrade
---
+++ src/components/Gallery.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Gallery() {
+return(
+    <div class="draw-trade"> Draw trade</div>
+);
+}
+
+export default Gallery
