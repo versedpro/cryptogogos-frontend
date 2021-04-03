e342e827cc44ce210f7ec71036fb0f84387b429a
Hossam Saraya <hossam.saraya@gmail.com>
2021-04-03 16:41:48 +0200

    update ui

---
+++ src/components/Home.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const HomeWrapper = styled.div`
+
+input.subscribe-email {
+    width: 50%;
+    margin: 0 auto;
+    border-radius: 10px;
+    padding-left: 1.5rem;
+    font-size: 1.5rem;
+    padding-top: 8px;
+}
+`
