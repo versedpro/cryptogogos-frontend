8073d733c8f3a704a370555147f29f5ad90bb983
versedpro <ultraj0330@gmail.com>
2021-04-10 00:43:30 +0800

    Update folder structure and distribution styled

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import 'react-multi-carousel/lib/styles.css'
 import * as S from './Home.styled'
 import Heading from './home_components/Heading'
-import Distribution from './home_components/Distribution'
+import Distribution from './Distribution/Distribution'
 import OurMission from './home_components/OurMission'
 
 
