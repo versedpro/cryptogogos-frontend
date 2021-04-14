682e8244d67925495ce621be947c309289751f5e
versedpro <ultraj0330@gmail.com>
2021-04-14 09:32:28 +0800

    Get token metadata

--- src/components/GogoList/GogoList.js
+++ src/components/GogoList/GogoList.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import * as S from './styled'
 import Web3 from 'web3'
 import ABI from '../../utils/contract.abi.json'
+import {
+    addPurchase,
+    addTransaction, confirmMint,
+    createMintRequest,
+    getDrawCard, getTokenMetadata,
+    registerUser,
+} from '../../utils/api'
 
 require('dotenv').config()
 
@@ -25,5 +32,70 @@ const loadContract = () => {
     return new window.web3.eth.Contract(ABI, process.env.REACT_APP_CONTRACT_ADDRESS) //testnet
 }
 
-const GogoList = () => {
+const GogoList = (props) => {
+    const [account, setAccount] = React.useState()
+    const [balance, setBalance] = React.useState()
+    const [tokens, setTokens] = React.useState([])
+
+    const loadAccount = async () => {
+        const web3 = window.web3
+
+        const accounts = await web3.eth.getAccounts()
+        setAccount(accounts[0])
+        const networkId = await web3.eth.net.getId()
+        console.log('networkId', networkId)
+    }
+
+    useEffect(async () => {
+        await loadWeb3()
+        await loadContract()
+        console.log('owner address: ', props.ownerAddress)
+        if (props.ownerAddress === undefined) {
+            await loadAccount()
+        } else {
+            setAccount(props.ownerAddress)
+        }
+    }, [])
+
+    useEffect(() => {
+        if (account !== undefined) {
+            getBalance()
+        }
+    }, [account])
+
+    useEffect(() => {
+        if (balance !== undefined) {
+            getTokens()
+        }
+    }, [balance])
+
+    const getBalance = async () => {
+        window.contract = await loadContract()
+        if (props.ownerAddress) {
+            setAccount(props.ownerAddress)
+        }
+        
+        const balance = await window.contract.methods.balanceOf(account).call()
+
+        setBalance(balance)
+    }
+
+    const getTokens = async () => {
+        const tokenList = []
+        for (let i = 0; i < balance; i++) {
+            tokenList.push(i)
+        }
+
+        await Promise.all(tokenList.map(async index => {
+            const tokenId = await window.contract.methods.tokenOfOwnerByIndex(account, index).call()
+            const {data: metadata } = await getTokenMetadata(tokenId)
+            tokens.push({ index: index, tokenId: tokenId, metaData: metadata })
+        }))
+    }
+
+    return (
+        <div>GogoList</div>
+    )
 }
+
+export default GogoList
