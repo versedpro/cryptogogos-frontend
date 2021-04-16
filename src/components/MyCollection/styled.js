d60bff6b1e785cc58e5aa9b0e198ba920b3f5dd8
versedpro <ultraj0330@gmail.com>
2021-04-16 22:42:47 +0800

    Updated package.json tab spaces, MyColleciton page styled component, added .env file

--- src/components/MyCollection/styled.js
+++ src/components/MyCollection/styled.js
@@ -1,12 +1,4 @@
 import styled from 'styled-components'
-import bg from '../../images/rocket-body-path.png'
-
-const tribeColors = {
-    Orange: 'darkorange',
-    Red: 'tomato',
-    Purple: 'blueviolet',
-    Green: '#3cc071',
-}
 
 export const MyCollectionWrapper = styled.div`
     .heading-section h2 {
